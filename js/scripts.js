f2c96c71700794b9f7b035052bcfb379e3576586
ika17s <86973632+ika17s@users.noreply.github.com>
2022-01-19 17:16:21 +0400

    main

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+}, 1200);
+
+var x = document.getElementById("wlc")
+
+    var date = new Date();  
+    var hour = date.getHours();    
+    if (hour < 12) {  
+      x.innerHTML = "Good morning"; 
+
+    } else if (hour < 17) {  
+      x.innerHTML = "Good afternoon";  
+    }
+     else {  
+      x.innerHTML = "Good Evening";  
+    } 
+
+    //------------//
+     
+ 
+//load script
+$(window).on("load",function(){
+     $(".loader-wrapper").fadeOut("slow");
+});
+
+
+
+//scroll script
+window.onscroll = function() {scrollFunctionside()};
+
+function scrollFunctionside() {
+  var x = document.getElementById("banner")
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("sidenav").style.left = "0px";
+    x.style.filter = "blur(0px)";
+  } else {
+    document.getElementById("sidenav").style.left = "-250px";
+    x.style.filter = "blur(5px)";
+  }
+}
+
+var l = document.getElementById('img')
+window.onload = function(){
+   setInterval(function(){
+       l.style.opacity = "1.0";
+   }, 1400);
+};
+
+
+//-----------------
